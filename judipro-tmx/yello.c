1d44d0119d33b9e29e079be1d6d13d9ad2f016a4
JudiciousPro <unengej@gmail.com>
2022-12-20 21:59:55 +0100

    My first commit

---
+++ judipro-tmx/yello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *Main - prints 'hello world'
+ *Return - displays 0 as successful
+*/
+int main(void)
+	{      char name[]="Jude F. Uneng";
+		int x=35;
+		float y=24.45;
+		char a='B';
+		printf("Say to him/her:\n");
+		printf("Hello World\n");
+		return (0);
+	}
