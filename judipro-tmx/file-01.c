052504c8211d00da106142e067bd6444d346eca4
JudiciousPro <unengej@gmail.com>
2022-12-21 08:55:54 +0100

    Added file-01.c and file-01.exe

---
+++ judipro-tmx/file-01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main: prints the contents of the program
+ * Return: produces 0 if program runs successfully
+ */
+int main(void)
+	{ 	printf("2*3\n");
+		printf("I am progressing in c programming\n");
+		printf("It's only a matter of small time before I turn pro!\n");
+		return (0);
+	}
